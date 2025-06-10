fd8c8ceda1e948cf79b3b4e5b67924ce7e61790a
drystee <106842887+drystee@users.noreply.github.com>
2025-06-10 10:11:09 +0100

    hi

--- script.js
+++ script.js
@@ -1,41 +1,4 @@
-    particlesJS("particles-js", {
-    "particles": {
-      "number": {
-        "value": 60,
-        "density": {
-          "enable": true,
-          "value_area": 800
-        }
-      },
-      "color": {
-        "value": "#facc15"
-      },
-      "shape": {
-        "type": "circle"
-      },
-      "opacity": {
-        "value": 0.5
-      },
-      "size": {
-        "value": 3
-      },
-      "move": {
-        "enable": true,
-        "speed": 1.5
-      }
-    },
-    "interactivity": {
-      "detect_on":"canvas",
-      "events": {
-        "onhover": {
-          "enable": false,
-          "mode": "repulse"
-        }
-      }
-    },
-    "retina_detect": false
-  });
-  
+
   const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
@@ -56,3 +19,71 @@
       learnMore.classList.remove('opacity-0', 'pointer-events-none');
     }
   });
+
+const carouselElement = document.getElementById('carousel-example');
+
+const items = [
+    {
+        position: 0,
+        el: document.getElementById('carousel-item-1'),
+    },
+    {
+        position: 1,
+        el: document.getElementById('carousel-item-2'),
+    },
+    {
+        position: 2,
+        el: document.getElementById('carousel-item-3'),
+    },
+    {
+        position: 3,
+        el: document.getElementById('carousel-item-4'),
+    },
+];
+
+// options with default values
+const options = {
+    defaultPosition: 1,
+    interval: 3000,
+
+    indicators: {
+        activeClasses: 'bg-white dark:bg-gray-800',
+        inactiveClasses:
+            'bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800',
+        items: [
+            {
+                position: 0,
+                el: document.getElementById('carousel-indicator-1'),
+            },
+            {
+                position: 1,
+                el: document.getElementById('carousel-indicator-2'),
+            },
+            {
+                position: 2,
+                el: document.getElementById('carousel-indicator-3'),
+            },
+            {
+                position: 3,
+                el: document.getElementById('carousel-indicator-4'),
+            },
+        ],
+    },
+
+    // callback functions
+    onNext: () => {
+        console.log('next slider item is shown');
+    },
+    onPrev: () => {
+        console.log('previous slider item is shown');
+    },
+    onChange: () => {
+        console.log('new slider item has been shown');
+    },
+};
+
+// instance options object
+const instanceOptions = {
+  id: 'carousel',
+  override: true
+};
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        raleway: ['"Raleway"', 'sans-serif'],
+        edu: ['"Edu NSW ACT Hand Pre"', 'cursive'],
+      },
+    },
+  },
+};
